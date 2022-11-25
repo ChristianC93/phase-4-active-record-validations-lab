208f1446b5245f4af8853fa15c2e0d8178fd702d
Christian Castillo <castillochristian3@gmail.com>
2022-11-25 13:43:24 -0500

    commit tree 40e30f2bf36a2136f644493307fc1e1cd08264d9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 } 
+    validates :summary, length: { maximum: 250 } 
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :title_must_be_clickbait
+
+
+    
+    def title_must_be_clickbait
+        clickbait_words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        if title.present? && title.include?(clickbait_words[0] || clickbait_words[1] || clickbait_words[2] || clickbait_words[3]) == false
+            errors.add(:title, "is not clickbait-y!")
+        end
+    end
 end
